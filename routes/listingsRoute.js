d335e863bfa9b6fa7f4750a387a41a37ebe76067
alanlinsanity <alan.linyh@gmail.com>
2022-05-28 15:38:11 +0800

    added show route backend for indiv listings

--- routes/listingsRoute.js
+++ routes/listingsRoute.js
@@ -128,7 +128,7 @@ router.get("/all", async (req, res) => {
 
 router.get("/:id", async (req, res) => {
   try {
-    const listing = await Listing.findOne({ id: req.params.id });
+    const listing = await Listing.findOne({ _id: req.params.id });
     res.send(listing);
   } catch (error) {
     return res.status(400).json({ message: error})
