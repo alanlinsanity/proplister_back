55a481859b7a663b5a45b2bf78b8beb675d14111
alanlinsanity <alan.linyh@gmail.com>
2022-06-02 19:54:15 +0800

    feat: redirect after delete route

--- routes/usersRoute.js
+++ routes/usersRoute.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const mongoose = require("mongoose");
+
 const mongoURL = `mongodb+srv://alanlinsanity:32s9221683g@cluster0.vq2et.mongodb.net/free2list`;
 
 mongoose.connect(mongoURL, { useUnifiedTopology: true, useNewUrlParser: true });
